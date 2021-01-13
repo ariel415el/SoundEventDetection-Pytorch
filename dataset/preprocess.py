Fix BCE weights; Disable wrong spectogram mixing; Use average cahnnel; model output logits

dataset/preprocess.py
@@ -88,7 +88,10 @@ def read_multichannel_audio(audio_path, target_fs=None):
     if len(multichannel_audio.shape) == 1:
         multichannel_audio = multichannel_audio.reshape(-1, 1)
     if multichannel_audio.shape[1] < cfg.audio_channels:
+        print(multichannel_audio.shape[1])
         multichannel_audio = np.repeat(multichannel_audio.mean(1).reshape(-1, 1), cfg.audio_channels, axis=1)
+    elif cfg.audio_channels == 1:
+        multichannel_audio = multichannel_audio.mean(1).reshape(-1, 1)
     elif multichannel_audio.shape[1] > cfg.audio_channels:
         multichannel_audio = multichannel_audio[:, :cfg.audio_channels]
 
@@ -98,7 +101,7 @@ def read_multichannel_audio(audio_path, target_fs=None):
 
         multichannel_audio = np.array(
             [librosa.resample(multichannel_audio[:, i], orig_sr=sample_rate, target_sr=target_fs) for i in range(channels_num)]
-                                        ).T
+        ).T
 
     return multichannel_audio, target_fs
 

models.py
@@ -195,12 +195,16 @@ class Cnn_AvgPooling(nn.Module):
         x = torch.mean(x, dim=3)    # (batch_size, channels_num, time_steps)
         x = x.transpose(1, 2)   # (batch_size, time_steps, channels_num)
 
-        event_output = torch.sigmoid(self.event_fc(x))  # (batch_size, time_steps, classes_num)
+        # event_output = torch.sigmoid(self.event_fc(x))  # (batch_size, time_steps, classes_num)
+        event_output = self.event_fc(x)  # (batch_size, time_steps, classes_num)
 
         # Interpolate
         event_output = interpolate(event_output, 2**(self.num_pools))
 
         return event_output
+
+    def logits(self, x):
+        return torch.sigmoid(self.forward(x))
 
     def model_description(self):
         print("Model description")

train.py
@@ -2,7 +2,7 @@ import os
 import argparse
 from tqdm import tqdm
 from torch import optim
-from utils import binary_crossentropy, ProgressPlotter, calculate_metrics, plot_debug_image, f_score
+from utils import clip_and_aply_criterion, ProgressPlotter, calculate_metrics, plot_debug_image, f_score
 from models import *
 import config as cfg
 from dataset.data_generator import get_film_clap_generator, get_tau_sed_generator, DataGenerator, cfg_descriptor
@@ -26,19 +26,19 @@ def eval(model, data_generator, outputs_dir, iteration, device, limit_val_sample
             model.eval()
             output = model(mel_features.to(device).float()).cpu()
 
-        loss = binary_crossentropy(output, target.float())
+        loss = clip_and_aply_criterion(output, target.float())
 
-        output = output.numpy()
+        output_logits = torch.sigmoid(output).numpy()
         target = target.numpy()
 
-        recal_vals, precision_vals, AP = calculate_metrics(output, target)
+        recal_vals, precision_vals, AP = calculate_metrics(output_logits, target)
 
         losses.append(loss.item())
         recal_sets.append(recal_vals)
         precision_sets.append(precision_vals)
         APs.append(AP)
 
-        debug_outputs.append(output)
+        debug_outputs.append(output_logits)
         debug_targets.append(target)
         debug_inputs.append(mel_features)
         debug_file_names.append(file_name)
@@ -62,7 +62,7 @@ def eval(model, data_generator, outputs_dir, iteration, device, limit_val_sample
 
 
 def train(model, data_generator, num_steps, lr, log_freq, outputs_dir, device):
-    lr_decay_freq = 100
+    lr_decay_freq = 200
     plotter = ProgressPlotter()
     os.makedirs(os.path.join(outputs_dir, 'checkpoints'), exist_ok=True)
 
@@ -77,7 +77,7 @@ def train(model, data_generator, num_steps, lr, log_freq, outputs_dir, device):
         # forward
         model.train()
         batch_outputs = model(batch_features.to(device).float())
-        loss = binary_crossentropy(batch_outputs, event_labels.to(device).float())
+        loss = clip_and_aply_criterion(batch_outputs, event_labels.to(device).float())
 
         # Backward
         optimizer.zero_grad()
@@ -120,11 +120,12 @@ if __name__ == '__main__':
     parser.add_argument('--dataset_dir', type=str, default='../data', help='Directory of dataset.')
     parser.add_argument('--outputs_root', type=str, default='training_dir')
     parser.add_argument('--ckpt', type=str, default='')
-    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--lr', type=float, default=0.000003)
     parser.add_argument('--val_perc', type=float, default=0.15)
-    parser.add_argument('--num_train_steps', type=int, default=10000)
+    parser.add_argument('--num_train_steps', type=int, default=30000)
     parser.add_argument('--log_freq', type=int, default=500)
+    parser.add_argument('--train_tag', type=str, default='')
     parser.add_argument('--device', default='cuda:0', type=str)
     parser.add_argument('--force_preprocess', action='store_true', default=False)
     parser.add_argument('--augment_data', action='store_true', default=False)
@@ -135,8 +136,8 @@ if __name__ == '__main__':
 
     device = torch.device("cuda:0" if torch.cuda.is_available() and args.device == "cuda:0" else "cpu")
     print(device)
-    model = Cnn_AvgPooling(cfg.classes_num, model_config=[(64, 2), (128, 2), (256, 2), (512, 1)]).to(device)
-    model.model_description()
+    model = Cnn_AvgPooling(cfg.classes_num, model_config=[(32,2), (64,2), (128,2), (128,1)]).to(device)
+    # model = MobileNetV1(cfg.classes_num).to(device)
     if args.ckpt != '':
         checkpoint = torch.load(args.ckpt, map_location=device)
         model.load_state_dict(checkpoint['model'])
@@ -149,13 +150,15 @@ if __name__ == '__main__':
                                    batch_size=args.batch_size,
                                    augment_data=args.augment_data,
                                    balance_classes=args.balance_classes,
-                                   val_descriptor=args.val_perc)
+                                   val_descriptor='DyingWithYou')
 
-    train_name = f"{dataset_name}_cfg({cfg_descriptor})_b{args.batch_size}_lr{args.lr}"
+    train_name = f"{dataset_name}_cfg({cfg_descriptor})_b{args.batch_size}_lr{args.lr}_{args.train_tag}"
     if args.balance_classes:
         train_name += "_BC"
     if args.augment_data:
         train_name += "_AD"
+
+    model.model_description()
 
     train(model, data_generator,
           num_steps=args.num_train_steps,

utils.py
@@ -1,11 +1,10 @@
+import os
 import numpy as np
 from matplotlib import pyplot as plt
-from torch.nn import functional as F
-from collections import defaultdict
-import os
 
-from config import tau_sed_labels, frames_per_second, classes_num, mel_bins
-
+from torch import tensor
+from config import frames_per_second, classes_num, mel_bins
+from torch.nn.functional import binary_cross_entropy_with_logits
 eps = np.finfo(np.float).eps
 
 
@@ -40,7 +39,7 @@ def compute_recall_precision(O, T):
     return recall, prec
 
 
-def binary_crossentropy(output, target, p_ones=0.7):
+def clip_and_aply_criterion(output, target):
     '''
     Binary crossentropy between output and target.
 
@@ -53,10 +52,11 @@ def binary_crossentropy(output, target, p_ones=0.7):
     N = min(output.shape[1], target.shape[1])
     clipped_output = output[:, 0: N, :]
     clipped_target = target[:, 0: N, :]
-    import torch
-    weight = torch.tensor([1 - p_ones, p_ones])
-    weight_ = weight[clipped_target.data.view(-1).long()].view_as(clipped_target)
-    return F.binary_cross_entropy(clipped_output, clipped_target, weight=weight_.to(target.device))
+
+    # criterion = BCEWithLogitsLoss(pos_weight=tensor([1])).to(target.device)
+    # return criterion(clipped_output, clipped_target)
+    return binary_cross_entropy_with_logits(clipped_output, clipped_target, pos_weight=tensor([10]).to(output.device))
+
 
 
 def f_score(recll, precision, precision_importance_factor=1):
@@ -68,7 +68,6 @@ class ProgressPlotter:
         self.train_buffer = []
         self.train_avgs = []
         self.val_avgs = []
-        self.fh_score_avgs = []
         self.f1_score_avgs = []
         self.f5_score_avgs = []
         self.AP_avgs = []
@@ -84,7 +83,6 @@ class ProgressPlotter:
         self.AP_avgs.append(np.mean(APs))
         self.last_recal_vals = np.mean(recal_sets, axis=0)
         self.last_precision_vals = np.mean(precision_sets, axis=0)
-        self.fh_score_avgs.append(self.last_precision_vals[0])
         f1_scores = f_score(self.last_precision_vals, self.last_recal_vals, precision_importance_factor=1)
         f5_scores = f_score(self.last_precision_vals, self.last_recal_vals, precision_importance_factor=5)
         self.f1_score_avgs.append(np.max(f1_scores))
@@ -112,7 +110,6 @@ class ProgressPlotter:
     def plot_metrics(self, plot_path):
         plt.plot(np.arange(len(self.f1_score_avgs)), self.f1_score_avgs, color='blue', label='Max f1 scroe')
         plt.plot(np.arange(len(self.f5_score_avgs)), self.f5_score_avgs, color='green', label='Max f5 scroe')
-        plt.plot(np.arange(len(self.fh_score_avgs)), self.fh_score_avgs, color='red', label='Precision in highest recall')
         plt.plot(np.arange(len(self.AP_avgs)), self.AP_avgs, color='orange', label='Average precision')
         plt.title("Metrics")
         x_indices = np.arange(0, len(self.iterations), max(len(self.iterations) // 5, 1))